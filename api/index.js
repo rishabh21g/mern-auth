25a1fe0d69fdb8ac9f0a72bc2d8705b87f7122f1
rishabh21g <biasedengineer@gmail.com>
2025-03-26 16:08:20 -0400

    DB connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,32 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
 
+
+dotenv.config({
+    path:"./.env"
+})
 const app = express()
+console.log(process.env.MONGO)
+
+
+const connectDB = async ()=>{
+    try{
+      const connectionInstance = await mongoose.connect(`${process.env.MONGO}/mern-auth`)
+      console.log("Success", connectionInstance.connection.host)
+    
+    }catch(err){
+        console.error("ERROR" , err.message)
+        process.exit(1)
+    }
+
+}
+connectDB().then(()=>{
+    app.listen(process.env.PORT||8000, ()=>{
+        console.log(`Server is listening on port ${process.env.PORT}`)
+    })
+
+})
 
 
 
